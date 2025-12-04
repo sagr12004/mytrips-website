9e11b126ad4ef08001035cddcaef595118a5c91d
sagr12004 <sagarssavadatti2004@gmail.com>
2025-12-04 21:33:13 +0530

    Intial commit

---
+++ supabase.js
@@ -0,0 +1,6 @@
+// supabase.js
+// <-- REPLACE these with your real values from Supabase settings -->
+const SUPABASE_URL = "https://mqiytonrkakicslraxjq.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xaXl0b25ya2FraWNzbHJheGpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3NjgxNTIsImV4cCI6MjA4MDM0NDE1Mn0.JK-X_-nctQ_e2CbqbuCwOAJ5oKVEi07a7deCKyWJpSA";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
